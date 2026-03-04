af4c4b2ad301e87277bb75fda7fc4c95c8879695
Frankie Wang <git@frankie.wang>
2026-03-04 03:39:00 +0000

    refactor: implement norm multiplicativity and properties for quadratic number fields

--- QuadNumberField/FieldInstance.lean
+++ QuadNumberField/FieldInstance.lean
@@ -4,6 +4,7 @@ Released under Apache 2.0 license as described in the file LICENSE.
 Authors: Frankie Wang
 -/
 import QuadNumberField.Param
+import Mathlib.NumberTheory.NumberField.Basic
 
 abbrev QuadNumberField (d : ℤ) [QuadFieldParam d] : Type := Qsqrtd (d : ℚ)
 
@@ -14,3 +15,29 @@ instance {d : ℤ} [QuadFieldParam d] : Field (QuadNumberField d) := by
     exact (not_isSquare_int d) (Rat.isSquare_intCast_iff.mp hsqQ)
   ⟩
   infer_instance
+
+-- /-- A number field is a field which has characteristic zero and is finite
+-- dimensional over ℚ. -/
+-- @[stacks 09GA]
+-- class NumberField (K : Type*) [Field K] : Prop where
+--   [to_charZero : CharZero K]
+--   [to_finiteDimensional : FiniteDimensional ℚ K]
+
+
+instance {d : ℤ} [QuadFieldParam d] : NumberField (QuadNumberField d) where
+  to_charZero := by infer_instance
+  to_finiteDimensional := by
+    have hmod :
+        (Algebra.toModule : Module ℚ (QuadNumberField d)) =
+          QuadraticAlgebra.instModule := by
+      refine Module.ext' _ _ ?_
+      intro r x
+      rw [Algebra.smul_def]
+      rw [show (algebraMap ℚ (QuadNumberField d) r) = QuadraticAlgebra.C r by
+            ext <;> simp [QuadNumberField]]
+      rw [QuadraticAlgebra.C_mul_eq_smul]
+    letI : Module ℚ (QuadNumberField d) := QuadraticAlgebra.instModule
+    have hfinite : Module.Finite ℚ (QuadNumberField d) := by
+      infer_instance
+    exact hmod ▸ hfinite
+
